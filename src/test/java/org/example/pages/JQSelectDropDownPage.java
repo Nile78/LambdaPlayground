ad1a2a837f3b4e3211427708e51b73220b2c8a9f
Nile78 <elin.ohlson.78@gmail.com>
2023-05-21 10:36:43 +0200

    First commit

---
+++ src/test/java/org/example/pages/JQSelectDropDownPage.java
@@ -0,0 +1,14 @@
+package org.example.pages;
+
+import org.openqa.selenium.By;
+
+public class JQuerySelectDropdownPage extends BasePage {
+
+    private dropDownWithSearchBox = By.
+
+
+    public dropDownWithSearchBox_Input() {
+
+    }
+
+}
